8609c422a4ee545759b2c966014caa53416abaaf
SumanX22 <suman.mesaha22@gmail.com>
2021-06-03 00:59:44 +0530

    changed the api hit url

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://api.cloutouts.com";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://api.cloutouts.com";
+// const BASE_URL = "http://localhost:5000";
 const API_BASE_URL = `${BASE_URL}/v1`;
 
 const endPoints = {
